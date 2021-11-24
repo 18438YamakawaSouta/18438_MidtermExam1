1929a525f53a79496898bf8fc56b2e937eca3436
18438YamakawaSotarou <18438@toba-cmt.ac.jp>
2021-11-24 16:02:35 +0900

    コードを映しました

--- udp_s.py
+++ udp_s.py
@@ -5,4 +5,3 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as A11:
     while True:
         RECVdata, fromaddr = A11.recvfrom(1024)
         print('data: {}, addr: {}'.format(RECVdata, fromaddr))
-        a
